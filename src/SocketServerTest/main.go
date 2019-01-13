ba5ecf933903836107ab60ccf3ce63a38048987a
lw000 <lw000>
2019-01-13 15:55:42 +0800

    更新

--- src/SocketServerTest/main.go
+++ src/SocketServerTest/main.go
@@ -3,55 +3,79 @@ package main
 
 import (
 	"fmt"
+	log "github.com/thinkboy/log4go"
 	"net"
 	"os"
 	"time"
-
-	log "github.com/thinkboy/log4go"
 )
 
 func handleClient(conn net.Conn) {
-	defer conn.Close()
+	defer func() {
+		err := conn.Close()
+		if err != nil {
 
-	buf := make([]byte, 100)
+		}
+	}()
+
+	var (
+		n   int
+		err error
+		buf []byte
+	)
+
+	buf = make([]byte, 1024)
 
 	for {
-		n, err := conn.Read(buf)
-
+		n, err = conn.Read(buf)
 		if err != nil {
-			fmt.Println("conn closed")
-			return
+			log.Debug(("connected closed"))
+			break
+		}
+		if n > 0 {
+
 		}
 
-		fmt.Println("recv msg:", string(buf[0:n]))
+		log.Debug("read:%s", string(buf[0:n]))
 
-		conn.Write([]byte("world!"))
+		n, err = conn.Write([]byte(time.Now().Format("2006-01-02 15:04:05")))
+		if err != nil {
+			log.Debug(("connected closed"))
+			break
+		}
+
+		if n > 0 {
+
+		}
 	}
 }
 
 func checkError(err error) {
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Fatal error: %s", err.Error())
+		fmt.Printf("Fatal error: %s\n", err.Error())
 		os.Exit(1)
 	}
 }
 
 func main() {
-	listen_sock, err := net.Listen("tcp", ":12563")
-	defer listen_sock.Close()
-
+	s, err := net.Listen("tcp", ":7777")
 	checkError(err)
 
-	log.Debug(time.Now())
+	defer func() {
+		err := s.Close()
+		if err != nil {
+
+		}
+	}()
+
 	log.Debug("server start... port:[12563]")
 
 	for {
-		conn, err := listen_sock.Accept()
+		conn, err := s.Accept()
 		if err != nil {
 			continue
 		}
 
-		log.Debug(conn.RemoteAddr().String(), " tcp connect success")
+		log.Debug("[%s]", conn.RemoteAddr().String())
 
 		go handleClient(conn)
 	}
