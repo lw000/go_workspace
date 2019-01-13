ecc372de602e4498af879630860d049cd7a27896
lw000 <lw000>
2019-01-13 19:12:41 +0800

    清理无用代码

--- src/pb/build-[game platform].bat
+++ src/pb/build-[game platform].bat
@@ -1,2 +1,5 @@
-protoc.exe --go_out=./game ./game.proto
-protoc.exe --go_out=./platform ./platform.proto                                                  
+cd ../..
+set GOPATH=%cd%
+cd src/pb
+protoc --go_out=./game ./game.proto
+protoc --go_out=./platform ./platform.proto
--- src/pb/build-helloworld.bat
+++ src/pb/build-helloworld.bat
@@ -1,4 +1,4 @@
 cd ../..
 set GOPATH=%cd%
 cd src/pb
-protoc.exe --go_out=plugins=grpc:./helloworld ./helloworld.proto                                        
+protoc --go_out=plugins=grpc:./helloworld ./helloworld.proto
